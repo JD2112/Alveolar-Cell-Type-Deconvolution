ae305974eb47027d0dd0d2dd4e6aed291598a503
Jyotirmoy Das <jyotirmoy21@gmail.com>
2020-08-24 15:16:44 +0200

    Update Figure 4.R

--- Figure 4.R
+++ Figure 4.R
@@ -15,11 +15,9 @@ library(EpiDISH)
 # data(DummyBeta.m)
 referenceData <- read.table("refDataB1_CD3_HLADR.txt", stringsAsFactors = F, header = T, row.names = 1)
 methData <- read.table("Stockholm_normalizedValue_GSE133062.txt", stringsAsFactors = F, header = T, row.names = 1)
-out.l <- epidish(beta.m = methData.m, ref.m = refData.m, method = "RPC")$estF
 methData.m <- as.matrix(methData)
 refData.m <- as.matrix(referenceData)
 out.lB1 <- epidish(beta.m = methData.m, ref.m = refData.m, method = "RPC")$estF
-boxplot(out.l)
 boxplot(out.lB1, col = c("#FDD4D4", "#99E5E0"))
 
 referenceData <- read.table("refDataB2_CD3_HLADR.txt", stringsAsFactors = F, header = T, row.names = 1)
@@ -68,3 +66,124 @@ title(outer=outer,adj=adj,main="B5",cex.main=cex,col="black",font=1,line=line)
 #title(outer=outer,adj=adj,main="Average",cex.main=cex,col="black",font=1,line=line)
   mtext("Cell proportion", side = 2, outer = T, at = 0.5, cex = 1.3)
 mtext("Cell types", side=1, outer=T, at=0.5, cex = 1.3)
+
+
+#Figure 4b or Figure 5; Next part
+# Plan is to find how similar (in case of cell proportion) BAL and SI samples when BAL samples (CpGs) are used as
+# the reference dataset and SI samples (CpGs) are methData set
+
+referenceData <- read.table("refDataB1_CD3_HLADR.txt", stringsAsFactors = F, header = T, row.names = 1)
+methData <- read.table("TestDataB1_SI_CD3_HLADR.txt", stringsAsFactors = F, header = T, row.names = 1)
+methData.m <- as.matrix(methData)
+refData.m <- as.matrix(referenceData)
+out.testB1 <- epidish(beta.m = methData.m, ref.m = refData.m, method = "RPC")$estF
+boxplot(out.testB1, col = c("#FDD4D4", "#99E5E0"))
+
+referenceData <- read.table("refDataB2_CD3_HLADR.txt", stringsAsFactors = F, header = T, row.names = 1)
+methData <- read.table("TestDataB2_SI_CD3_HLADR.txt", stringsAsFactors = F, header = T, row.names = 1)
+methData.m <- as.matrix(methData)
+refData.m <- as.matrix(referenceData)
+out.testB2 <- epidish(beta.m = methData.m, ref.m = refData.m, method = "RPC")$estF
+boxplot(out.testB2, col = c("#FDD4D4", "#99E5E0"))
+
+referenceData <- read.table("refDataB3_CD3_HLADR.txt", stringsAsFactors = F, header = T, row.names = 1)
+methData <- read.table("TestDataB3_SI_CD3_HLADR.txt", stringsAsFactors = F, header = T, row.names = 1)
+methData.m <- as.matrix(methData)
+refData.m <- as.matrix(referenceData)
+out.testB3 <- epidish(beta.m = methData.m, ref.m = refData.m, method = "RPC")$estF
+boxplot(out.testB3, col = c("#FDD4D4", "#99E5E0"))
+
+referenceData <- read.table("refDataB4_CD3_HLADR.txt", stringsAsFactors = F, header = T, row.names = 1)
+methData <- read.table("TestDataB4_SI_CD3_HLADR.txt", stringsAsFactors = F, header = T, row.names = 1)
+methData.m <- as.matrix(methData)
+refData.m <- as.matrix(referenceData)
+out.testB4 <- epidish(beta.m = methData.m, ref.m = refData.m, method = "RPC")$estF
+boxplot(out.testB4, col = c("#FDD4D4", "#99E5E0"))
+
+referenceData <- read.table("refDataB5_CD3_HLADR.txt", stringsAsFactors = F, header = T, row.names = 1)
+methData <- read.table("TestDataB5_SI_CD3_HLADR.txt", stringsAsFactors = F, header = T, row.names = 1)
+methData.m <- as.matrix(methData)
+refData.m <- as.matrix(referenceData)
+out.testB5 <- epidish(beta.m = methData.m, ref.m = refData.m, method = "RPC")$estF
+boxplot(out.testB5, col = c("#FDD4D4", "#99E5E0"))
+
+#referenceData <- read.table("refDataAVG_CD3_HLADR.txt", stringsAsFactors = F, header = T, row.names = 1)
+#methData <- read.table("TestDataAVG_SI_CD3_HLADR.txt", stringsAsFactors = F, header = T, row.names = 1)
+#methData.m <- as.matrix(methData)
+#refData.m <- as.matrix(referenceData)
+#out.testAVG <- epidish(beta.m = methData.m, ref.m = refData.m, method = "RPC")$estF
+#boxplot(out.testAVG, col = c("#FDD4D4", "#99E5E0"))
+
+outer = FALSE
+line = 1
+cex = 1.3
+adj  = 0.025
+par(mfrow=c(3,2), tcl=-0.5, family="Times New Roman", omi=c(0.3,0.3,0,0), 
+    mar=c(2.3, 2.3, 1.8, 1.8), oma = c(2, 2, 1, 1))
+boxplot(out.testB1,  col = c("#FDD4D4", "#99E5E0"), font.family = "Times New Roman")
+title(outer=outer,adj=adj,main="B1",cex.main=cex,col="black",font=1,line=line)
+boxplot(out.testB2,  col = c("#FDD4D4", "#99E5E0"), font.family = "Times New Roman")
+title(outer=outer,adj=adj,main="B2",cex.main=cex,col="black",font=1,line=line)
+boxplot(out.testB3,  col = c("#FDD4D4", "#99E5E0"), font.family = "Times New Roman")
+title(outer=outer,adj=adj,main="B3",cex.main=cex,col="black",font=1,line=line)
+boxplot(out.testB4,  col = c("#FDD4D4", "#99E5E0"), font.family = "Times New Roman")
+title(outer=outer,adj=adj,main="B4",cex.main=cex,col="black",font=1,line=line)
+boxplot(out.testB5,  col = c("#FDD4D4", "#99E5E0"), font.family = "Times New Roman")
+title(outer=outer,adj=adj,main="B5",cex.main=cex,col="black",font=1,line=line)
+#boxplot(out.testAVG, col = c("#FDD4D4", "#99E5E0"), font.family = "Times New Roman")
+#title(outer=outer,adj=adj,main="Average",cex.main=cex,col="black",font=1,line=line)
+mtext("Cell proportion", side = 2, outer = T, at = 0.5, cex = 1.3)
+mtext("Cell types", side=1, outer=T, at=0.5, cex = 1.3)
+
+mB <- cor(data)
+mB1 <- as.matrix(B1_data)
+corrplot(mB, method = "pie")
+corrplot(mB, method = "number")
+boxplot(mB)
+
+SI <- grepl("_SI_", colnames(data))
+
+colo1 <- ifelse(SI, "#FDD4D4", "#99E5E0")
+par(mar = c(6.1, 4.1, 4.1, 4.1), # change the margins
+    lwd = 2, # increase the line thickness
+    cex.axis = 1.2 # increase default axis label size
+)
+boxplot(mB, xaxt = "n", yaxt = "n", col = colo1, notch = T)
+axis(side = 1, labels = F)
+axis(side = 2)
+text(x = 1:length(data),
+     y=par()$usr[3]-0.1*(par()$usr[4]-par()$usr[3]),
+     labels = colnames(data),
+     col = colo2,
+     family = "Times New Roman",
+     font.size = 12,
+     xpd = TRUE,
+     adj = c(0.6, -3.5),
+     offset = 1,
+     ## Rotate the labels by 35 degrees.
+     srt = 40,
+     cex = 1.1)
+abline(h=0.9, col = "red", lty = 3)
+#abline(v=10.5, col = "red", lty = 3)
+
+HLADR <- grepl("_HLADR", colnames(data))
+colo2 <- ifelse(HLADR, "blue", "black")
+par(mar = c(6.1, 4.1, 4.1, 4.1), # change the margins
+    lwd = 2, # increase the line thickness
+    cex.axis = 1.2 # increase default axis label size
+)
+vioplot(mB, col = colo1, xaxt = "n")
+text(x = 1:length(data),
+     y=par()$usr[3]-0.1*(par()$usr[4]-par()$usr[3]),
+     labels = colnames(data),
+     col = colo2,
+     family = "Times New Roman",
+     font.size = 12,
+     xpd = TRUE,
+     adj = c(0.6, -3.5),
+     offset = 1,
+     ## Rotate the labels by 35 degrees.
+     srt = 40,
+     cex = 1.1)
+abline(h=0.9, col = "red", lty = 3)
+
