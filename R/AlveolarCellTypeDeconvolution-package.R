84a2befcf50bd0bd6f54369923de6131e989d330
jd2112 <jyotirmoy21@gmail.com>
2026-03-16 13:42:48 +0100

    Restructure Alveolar-Cell-Type-Deconvolution as an R package

---
+++ R/AlveolarCellTypeDeconvolution-package.R
@@ -0,0 +1,8 @@
+#' Alveolar Cell Type Deconvolution
+#'
+#' This package contains scripts for alveolar cell type deconvolution.
+#' The scripts are located in the \code{inst/Rscripts} directory of the installed package.
+#'
+#' @name AlveolarCellTypeDeconvolution
+#' @docType package
+NULL
